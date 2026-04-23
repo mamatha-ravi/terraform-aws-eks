16a7f74a7cfbb50ba1770a00a4e78394d8043671
mamatha-ravi <mamata.r@gmail.com>
2026-04-23 17:01:11 +0530

    awsworkstation

--- 60-eks/provider.tf
+++ 60-eks/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "remote-state-aws-88s-dev" # Replace with your unique bucket name
+    bucket  = "remote-state-s3-88s-dev" # Replace with your unique bucket name
     key     = "roboshop-eks-eks"
     region  = "us-east-1"
     encrypt = true
